a4895249950eaa37ac913f4aa2ebe058b8d03786
Jack Booth <jack_booth@mac.com>
2024-11-04 14:46:17 +0000

    Updated AWS Cognito details

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,32 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>
 );
+
+import { Amplify } from "aws-amplify"
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolId: "<eu-west-2_aE9tWxtGk>",
+      userPoolClientId: "<idp:eu-west-2:136844321477>",
+      identityPoolId: "<userpool/eu-west-2_aE9tWxtGk>",
+      loginWith: {
+        email: true,
+      },
+      signUpVerificationMethod: "code",
+      userAttributes: {
+        email: {
+          required: true,
+        },
+      },
+      allowGuestAccess: true,
+      passwordFormat: {
+        minLength: 8,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireNumbers: true,
+        requireSpecialCharacters: true,
+      },
+    },
+  },
+})
